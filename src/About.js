ac2c173002f224314cf19fb54c8cb7275b4419de
Chris-Vitalis <cvitalis@uw.edu>
2018-11-26 13:51:09 -0800

    Add Bios

--- src/About.js
+++ src/About.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import { Header } from './App';
+import { chrisBio, ameliaBio } from './bios';
+
 
 class AboutPage extends Component {
   render () {
     return (
-      <Header/>
+      <div>
+        <Header/>
+        <Profile person="amelia" bio={ameliaBio}/>
+        <Profile person="chris" bio={chrisBio}/>
+      </div>
     )
   }
 }
+
+class Profile extends Component {
+  let
+  render(){
+    console.log(this.props.url);
+    return (
+      <div>
+        <div className={this.props.person + "Icon icon"}></div>
+        <p>{this.props.bio}</p>
+      </div>
+    )
+  }
+}
+
 
 export default AboutPage;
---
+++ src/bios.js
@@ -0,0 +1,12 @@
+export let chrisBio =  `Hey! My name is Christopher Vitalis and I am a first year
+ student at the University of Washington’s Information School. I’m majoring 
+ in Informatics with a focus in Human-Computer Interaction. I have a passion 
+ for designing user interfaces that present information both aesthetically and 
+ functionally as to give the user an experience that doesn’t leave them confused 
+ or frustrated. A quote from Jared Spool that I feel guides my philosophy on design
+  is “Good design, when it’s done well, becomes invisible. It’s only when it’s done 
+  poorly that we notice it.” In my spare time I enjoy finding new music and analyzing the 
+  different roles that non-traditional sounds play within songs. I also enjoy bouldering and finding new boba 
+  shops with my friends in search of the perfect tea. `;
+  
+  export let ameliaBio = `Here is her bio`;
