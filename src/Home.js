41e6631b515120dc47619a1078017d2fc2bc3f43
Chris-Vitalis <cvitalis@uw.edu>
2018-11-27 14:57:48 -0800

    Jumbotron

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import { Header } from './App';
 
+
 class HomePage extends Component {
   render () {
     return (
-      <Header/>
+      <div>
+      <Header/>    
+      <main>
+        <section className="why">
+          <div className="jumbotron">
+            <h2 className="display-4">Smart Card</h2>
+            <p class="lead">The 
+              <em> new </em>
+               way to study.
+            </p>
+            <hr className="my-4"></hr>
+            <a class="btn btn-lg get-started-button">Get Started</a>
+          </div>
+          
+        </section>
+      </main>
+    </div>
     )
   }
 }
