5c0b887b555efc2ae0b3a3e04a91386b5e5ede9b
Chris-Vitalis <cvitalis@uw.edu>
2018-12-04 12:39:56 -0800

    Add smart cards to home

--- src/Home.js
+++ src/Home.js
@@ -24,39 +24,56 @@ class HomePage extends Component {
               </div>
             </div>
             </section>
+
             <section className="more-info">
-                <h2 className="display-4">
-                <img className="icon-front" alt="Question mark icon" 
+            <div className="card-container">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front example-text">
+                  <img className="icon-front" alt="Person Icon" 
                 src="https://png.icons8.com/ios/100/000000/help-filled.png">
                 </img>
-                What is Smart Card
-              </h2>
-              <p>
-                  Smart Card is a website that can be used to help you study.
-                  Just enter in your typed up notes and Smart Card will create 
-                  <em> personalized</em> note cards!
-              </p>
+                    <h1 className="example-font">What is Smart Card?</h1>
+                  </div>
+                    <div class="flip-card-back">
+                      <p className="example-text">Smart Card is a website that can be used to help you study. 
+                      Just enter in your typed up notes and Smart Card will create 
+                      personalized note cards!</p> 
+                    </div>
+                </div>
+              </div>
 
-                <h2 className="display-4">
-                <img className="icon-front" alt="Person Icon" 
+              <div class="flip-card example-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front example-text">
+                  <img className="icon-front" alt="Person Icon" 
                 src="https://png.icons8.com/material-rounded/48/000000/user.png">
                 </img>
-                Who Made Smart Card
-              </h2>
-              <p>
-                Smart Card was developed by Amelia Shull and Christopher Vitalis 
-                in the Informatics major at the University of Washington. 
-              </p>
+                    <h1 className="example-font">Who made Smart Card?</h1>
+                  </div>
+                    <div class="flip-card-back">
+                      <p className="example-text">Smart Card was developed by Amelia Shull, Rachel Vuu, Yu Che Lin, and
+                        Christopher Vitalis in the Informatics major at the University of Washington.</p> 
+                    </div>
+                </div>
+              </div>
 
-                <h2 className="display-4">
-                <img className="icon-front" alt="Piggy Bank Icon" 
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front example-text">
+                  <img className="icon-front" alt="Person Icon" 
                 src="https://png.icons8.com/ios/50/000000/money-box-filled.png">
                 </img>
-                Is Smart Card Free?
-                </h2>
-              <p>
-                Yes! Smart Card is 100% free for everyone.
-              </p>
+                    <h1 className="example-font">Is Smart Card free</h1>
+                  </div>
+                    <div class="flip-card-back">
+                      <p className="example-text">Yes! Smart Card is 100% free!</p> 
+                    </div>
+                </div>
+              </div>
+              </div>
+
+
             </section>
         </main>
         <Footer/>
