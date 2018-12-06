974d7173b9541f791c39fc6e0ad319f182beaab2
amelia-shull <ashull@uw.edu>
2018-12-06 12:39:14 -0800

    update home a bit

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,31 @@ import { Link } from 'react-router-dom'
 
 class HomePage extends Component {
   render () {
+    let exampleCards = [
+      {
+        img: (<img className="icon-front" alt="Person Icon" 
+          src="https://png.icons8.com/ios/100/000000/help-filled.png"></img>),
+        front : (<h1 className="example-font">What is Smart Card?</h1>),
+        back: (<p className="example-text back-text-1">Smart Card is a website that can be used to help you study. 
+          Just enter in your typed up notes and Smart Card will create 
+          personalized note cards!</p> )
+      },
+      {
+        img: (<img className="icon-front" alt="Person Icon" 
+          src="https://png.icons8.com/material-rounded/48/000000/user.png"></img>),
+        front: (<h1 className="example-font">Who made Smart Card?</h1>),
+        back: (<p className="example-text back-text-2">Smart Card was developed by Amelia Shull, Rachel Vuu, Yu Che Lin, and
+          Christopher Vitalis in the Informatics major at the University of Washington.</p>)
+      },
+      {
+        img: (<img className="icon-front" alt="Person Icon" 
+          src="https://png.icons8.com/ios/50/000000/money-box-filled.png"></img>),
+        front: (<h1 className="example-font">Is Smart Card free?</h1>),
+        back: (<p className="example-text back-text-3">Yes! Smart Card is 100% free!</p>)
+      }
+    ];
     return (
       <div className="background-image">
-
         <Header/>
         <main>
           <section className="why">
@@ -25,60 +47,35 @@ class HomePage extends Component {
             </div>
             </section>
 
-            <section className="more-info">
+          <section className="more-info">
             <div className="card-container">
-              <div className="flip-card">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front example-text">
-                  <img className="icon-front" alt="Person Icon" 
-                src="https://png.icons8.com/ios/100/000000/help-filled.png">
-                </img>
-                    <h1 className="example-font">What is Smart Card?</h1>
-                  </div>
-                    <div className="flip-card-back">
-                      <p className="example-text back-text-1">Smart Card is a website that can be used to help you study. 
-                      Just enter in your typed up notes and Smart Card will create 
-                      personalized note cards!</p> 
-                    </div>
-                </div>
-              </div>
-
-              <div className="flip-card">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front example-text">
-                  <img className="icon-front" alt="Person Icon" 
-                src="https://png.icons8.com/material-rounded/48/000000/user.png">
-                </img>
-                    <h1 className="example-font">Who made Smart Card?</h1>
-                  </div>
-                    <div className="flip-card-back">
-                      <p className="example-text back-text-2">Smart Card was developed by Amelia Shull, Rachel Vuu, Yu Che Lin, and
-                        Christopher Vitalis in the Informatics major at the University of Washington.</p> 
-                    </div>
-                </div>
-              </div>
-
-              <div className="flip-card">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front example-text">
-                  <img className="icon-front" alt="Person Icon" 
-                src="https://png.icons8.com/ios/50/000000/money-box-filled.png">
-                </img>
-                    <h1 className="example-font">Is Smart Card free?</h1>
-                  </div>
-                    <div className="flip-card-back">
-                      <p className="example-text back-text-3">Yes! Smart Card is 100% free!</p> 
-                    </div>
-                </div>
-              </div>
-              </div>
-
-
-            </section>
+              <ExampleCard card={exampleCards[0]}></ExampleCard>
+              <ExampleCard card={exampleCards[1]}></ExampleCard>
+              <ExampleCard card={exampleCards[2]}></ExampleCard>
+            </div>{/* End card container*/}
+          </section>
         </main>
         <Footer/>
       </div>
     )
+  }
+}
+
+class ExampleCard extends Component {
+  render() {
+    return(
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front example-text">
+            {this.props.card.img}
+            {this.props.card.front}
+          </div>
+          <div className="flip-card-back">
+            {this.props.card.back}
+           </div>
+        </div>
+      </div>
+    );
   }
 }
 
